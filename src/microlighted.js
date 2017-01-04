86b1a9346c3894c33d354bd4d1d6b5db16660082
Torsten Stelling <stelling@neogen.info>
2017-01-04 21:07:57 +0100

    added new highlight functions with restructured code

--- src/microlighted.js
+++ src/microlighted.js
@@ -16,6 +16,260 @@
 }(this, function (exports) {
 	"use strict";
 
+	var highlight = function(element) {
+		var text = element.textContent;
+		var pos = 0;
+		var next1 = text[0];
+		var chr = 1;
+		var prev1;
+		var prev2;
+		var lastToken;
+		var lastTokenType;
+		var multichar;
+		var node;
+		var tokens = [];
+		var braces = [];
+
+		// current token type:
+		// - anything else (whitespaces / newlines)
+		// - linefeed (disabled for now)
+		// comment types
+		// - xml comment  <!-- -->
+		// - multiline comment /* */
+		// - single-line comment starting with two slashes //
+		// - single-line comment starting with hash #
+		// other
+		// - operator
+		// - opening braces
+		// - closing braces (after which '/' is division not regex)
+		// - (key)word (html, javascript, other)
+		// strings and regex
+		// - regex
+		// - string starting with "
+		// - string starting with '
+		// numbers
+		// - numbers with optional trailing characters
+		// - hex number with # and minimum of 3 characters
+		var tokenTypesCount = 0;
+		var tokenTypes = {
+			whitespace: tokenTypesCount++,
+			// linefeed: tokenTypesCount++,
+			operator: tokenTypesCount++,
+			openbrace: tokenTypesCount++,
+			closebrace: tokenTypesCount++,
+			keyword: tokenTypesCount++,
+			regex: tokenTypesCount++,
+			stringdouble: tokenTypesCount++,
+			stringsingle: tokenTypesCount++,
+			xmlcomment: tokenTypesCount++,
+			slashcomment: tokenTypesCount++,
+			slashstarcomment: tokenTypesCount++,
+			hashcomment: tokenTypesCount++,
+			number: tokenTypesCount++,
+			hexnumber: tokenTypesCount++,
+			none: tokenTypesCount++
+		}
+		// console.log(tokenTypes);
+
+		var token = '';
+		var tokenType = 0;
+
+		// clear content
+		element.innerHTML = '';
+
+		while (prev2 = prev1,
+			   // escaping if needed (with except for comments)
+			   // pervious character will not be therefore
+			   // recognized as a token finalize condition
+			   prev1 = ((tokenType<7)&&(prev1=='\\'))?1:chr // TODO tokenType abfrage anpassen
+		) {
+			chr = next1;
+			next1=text[++pos];
+			multichar = token.length>1;
+
+			if (!chr|| // end of content
+				// single-line comments end with a newline
+				(([tokenTypes.slashcomment,tokenTypes.hashcomment].indexOf(tokenType)>-1)&&(chr=='\n'))||
+				// finalize conditions for other token types
+				({
+					// whitespaces
+					whitespace: /\S/.test(chr),  // merged together
+					// linefeed: 1, // consist of a single character
+					// xml comment
+					xmlcomment: text[pos-4]+prev2+prev1 == '-->',
+					// multiline comment
+					slashstarcomment: prev2+prev1 == '*/',
+					// single line comment
+					slashcomment: false,
+					// hash comment
+					hashcomment: false,
+					// operators
+					operator: 1, // consist of a single character
+					// braces
+					openbrace: 1, // consist of a single character
+					closebrace: 1, // consist of a single character
+					// (key)word
+					keyword: !/[$\w]/.test(chr),
+					// regex
+					regex: (prev1 == '/' || prev1 == '\n') && multichar,
+					// string with "
+					stringdouble: prev1 == '"' && multichar,
+					// string with '
+					stringsingle: prev1 == "'" && multichar,
+					// number end
+					number: !/^\d+[a-z%]*$/.test(token+chr), // FIXME only html types at end...
+					// hex number
+					hexnumber: !/^#[0-9a-fA-F]+$/.test(token+chr)
+				}[Object.keys(tokenTypes)[tokenType]])) {
+
+				if (token) {
+					var tokenStruct = {
+						token: token,
+						tokenType: tokenType,
+						// multichar: multichar,
+						// pos: pos-1,
+						key: Object.keys(tokenTypes)[tokenType], // for debugging
+						class: []
+					};
+					tokens.push(tokenStruct);
+					// easy class creation
+					var value = {
+						xmlcomment: 'comment',
+						hashcomment: 'comment',
+						slashcomment: 'comment',
+						slashstarcomment: 'comment',
+						operator: 'operator',
+						openbrace: 'brace',
+						closebrace: 'brace',
+						keyword: 'keyword',
+						regex: 'string',
+						stringsingle: 'string',
+						stringdouble: 'string',
+						number: 'number',
+						hexnumber: 'number'
+					}[tokenStruct.key];
+					if (value!=undefined) tokenStruct.class.push(value);
+
+					if (/^(a(bstract|lias|nd|rguments|rray|s(m|sert)?|uto)|b(ase|egin|ool(ean)?|reak|yte)|c(ase|atch|har|hecked|lass|lone|ompl|onst|ontinue)|de(bugger|cimal|clare|f(ault|er)?|init|l(egate|ete)?)|do|double|e(cho|ls?if|lse(if)?|nd|nsure|num|vent|x(cept|ec|p(licit|ort)|te(nds|nsion|rn)))|f(allthrough|alse|inal(ly)?|ixed|loat|or(each)?|riend|rom|unc(tion)?)|global|goto|guard|i(f|mp(lements|licit|ort)|n(it|clude(_once)?|line|out|stanceof|t(erface|ernal)?)?|s)|l(ambda|et|ock|ong)|m(icrolight|odule|utable)|NaN|n(amespace|ative|ext|ew|il|ot|ull)|o(bject|perator|r|ut|verride)|p(ackage|arams|rivate|rotected|rotocol|ublic)|r(aise|e(adonly|do|f|gister|peat|quire(_once)?|scue|strict|try|turn))|s(byte|ealed|elf|hort|igned|izeof|tatic|tring|truct|ubscript|uper|ynchronized|witch)|t(emplate|hen|his|hrows?|ransient|rue|ry|ype(alias|def|id|name|of))|u(n(checked|def(ined)?|ion|less|signed|til)|se|sing)|v(ar|irtual|oid|olatile)|w(char_t|hen|here|hile|ith)|xor|yield)$/.test(token)) {
+						tokenStruct.class.push('token');
+					}
+
+					// html and /html items (buggy)
+					// FIXME does not work when attributes are there...
+					if ((token=='>')&&
+						(tokens[tokens.length-2].tokenType==tokenTypes.keyword)&&
+						(((tokens[tokens.length-3].token=='/')&&
+						  (tokens[tokens.length-4].token=='<'))||
+						 (tokens[tokens.length-3].token=='<'))
+					) {
+						if (tokens[tokens.length-3].token=='/') {
+							tokens[tokens.length-3].class = ['brace'];
+						}
+						// tokens[tokens.length-3].class.push('htmlbrace');
+						tokens[tokens.length-2].class.push('html');
+						// tokens[tokens.length-1].class.push('htmlbrace');
+					}
+
+					if ((token=='true')||
+						(token=='false')) {
+						tokenStruct.class.push('boolean');
+					}
+
+					if ((token=='php')&&
+						(tokens[tokens.length-2].token=='?')&&
+						(tokens[tokens.length-3].token=='<')) {
+						tokens[tokens.length-2].class = ['brace'];
+						tokenStruct.class = ['html'];
+					}
+					// console.log(tokenStruct);
+				}
+
+				// FIXME the last tokens...
+				lastTokenType = /*(tokenType&&(tokenType>=10))?*/tokenType;//:lastTokenType;
+				lastToken = /*(tokenType&&(tokenType>=10))?*/token.trim();//:lastToken;
+				token = '';
+
+				// determining the new token type (going up the
+				// list until matching a token type start
+				// condition)
+				tokenType = tokenTypesCount;
+				while (!{
+					// whitespace
+					whitespace: 1,
+					// linefeed
+					// linefeed: /\n/.test(chr),
+					// xml comment
+					xmlcomment: chr+next1+text[pos+1]+text[pos+2] == '<!--',
+					// multiline comment
+					slashstarcomment: chr+next1 == '/*',
+					// single-line comment
+					slashcomment: chr+next1 == '//',
+					// hash-style comment
+					hashcomment: chr == '#',
+					// operator
+					operator: /[\/\-+*=:|\\.,?!&@~]/.test(chr), // ; is removed
+					// openning braces
+					openbrace: /[\[(<{}]/.test(chr),
+					// closing braces
+					closebrace: /[\])>}]/.test(chr),
+					// (key)word
+					keyword: /[$\w]/.test(chr),
+					// regex
+					regex: (chr=='/')&&
+						// previous token was an opening brace or an operator
+						// (otherwise division, not a regex)
+						([tokenTypes.openbrace,tokenTypes.operator].indexOf(lastTokenType)>-1)&&
+						// workaround for xml closing tags
+						(prev1!='<'),
+					// string with "
+					stringdouble: chr=='"',
+					// string with '
+					stringsingle: chr=="'",
+					// numbers
+					number: /^[0-9]$/.test(chr),
+					// hex number min. 3 with #
+					hexnumber: /^#([0-9a-fA-F]){3}$/.test(chr+next1+text[pos+1]+text[pos+2])
+				}[Object.keys(tokenTypes)[--tokenType]]) {
+					// console.log(tokenType);
+					if (tokenType==0) break; // in case of an error ;)
+				};
+				// console.log(tokenType,Object.keys(tokenTypes)[tokenType],chr);
+			}
+
+			token += chr;
+			// break;
+		}
+
+		// console.log(tokens);
+		for (var i=0;i<tokens.length;i++) {
+			var token = tokens[i];
+			// console.log(token);
+			var node;
+			if (token.class.length>0) {
+				element.appendChild(
+					node = document.createElement('span')
+				).setAttribute('class',token.class.join(' '));
+				node.appendChild(document.createTextNode(token.token));
+			} else {
+				element.appendChild(
+					node = document.createTextNode(token.token)
+				)
+			}
+		}
+	}
+
+	var highlightElements = function(elements) {
+		var element;
+		for (var i = 0; element = elements[i++];) {
+			highlight(element);
+		}
+	}
+
+	var highlightClass = function(cls) {
+		var elements = document.getElementsByClassName(cls||'microlighted');
+
+		highlightElements(elements);
+	}
 
 	var reset = function(cls) {
 		// nodes to highlight
@@ -200,11 +454,17 @@
 		}
 	}
 
+	// old method
 	exports.reset = reset;
 
-	if (document.readyState == 'complete') {
-		reset();
-	} else {
-		window.addEventListener('load', function(){reset()}, 0);
-	}
+	// new methods
+	exports.highlight = highlight;
+	exports.highlightElements = highlightElements;
+	exports.highlightClass = highlightClass;
+
+	// if (document.readyState == 'complete') {
+	// 	reset();
+	// } else {
+	// 	window.addEventListener('load', function(){reset()}, 0);
+	// }
 }));
